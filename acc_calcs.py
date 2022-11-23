8238ec743e5c035964af61de622e1494278870dc
eelgavish <eelgavish@utexas.edu>
2022-11-23 15:57:41 -0600

    camdata focused

--- acc_calcs.py
+++ acc_calcs.py
@@ -9,8 +9,10 @@ from filtering import butter_filter
 
 #data = pd.read_csv("IMUdata/DrMeryskin1667249917.csv")
 data = pd.read_csv("IMUdata/DrVanardosanastomosis1667251523.csv")
-fing1 = pd.read_csv("CAMdata/dr_vernaldos_anastomosis_left.csv")
-fing2 = pd.read_csv("CAMdata/dr_vernaldos_anastomosis_right.csv")
+#fing1 = pd.read_csv("CAMdata/dr_vernados_anastomosis_left_all_frames.csv")
+#fing2 = pd.read_csv("CAMdata/dr_vernados_anastomosis_right_all_frames.csv")
+fing1 = pd.read_csv("CAMdata/dr_mery_anastomosis_left_all_frames.csv")
+fing2 = pd.read_csv("CAMdata/dr_mery_anastomosis_right_all_frames.csv")
 # Time domain
 time = data['Time'].to_numpy()
 
@@ -28,15 +30,17 @@ print("Time: "+str(time[rnge[1]]-time[rnge[0]]))
 acc1 = data[['accX1','accY1','accZ1']].values
 acc2 = data[['accX2','accY2','accZ2']].values*9.81
 
+"""
 # subtract out gravity via orientation
 # end orientation
-acc1x = np.mean(acc1[500:600,0])
-acc1y = np.mean(acc1[500:600,1])
-acc1z = np.mean(acc1[500:600,2])
+acc1x = np.mean(np.flip(acc1)[0:100,0])
+acc1y = np.mean(np.flip(acc1)[0:100,1])
+acc1z = np.mean(np.flip(acc1)[0:100,2])
 acc1t = np.array([acc1x,acc1y,acc1z])
 
 g = [0,0,-9.8]
 Ra = rm.align(acc1t,g)
+g = [0,0,np.linalg.norm(acc1t)]
 dt = time[1] - time[0]
 
 w1x = np.flip(data['wX1'].values)
@@ -51,9 +55,19 @@ for i in range(0,length):
     else:
         r1 = np.eye(3)
     #r1a = r1a.append(pd.Series({i+1: [np.dot(r1a.get(i)[0],r1)]}))
-
     Ra = np.dot(Ra,r1)
+    #print(np.dot(Ra,acc1[length-1-i])-g)
     acc1[length-1-i] = np.dot(Ra,acc1[length-1-i])-g
+
+# Cheating to get the numbers to go down
+acc1x = np.mean(acc1[0:4000,0])
+acc1y = np.mean(acc1[0:4000,1])
+acc1z = np.mean(acc1[0:4000,2])
+
+acc1[:,0] = acc1[:,0] - acc1x
+acc1[:,1] = acc1[:,1] - acc1y
+acc1[:,2] = acc1[:,2] - acc1z
+
 
 # filter data
 N = 3
@@ -88,6 +102,7 @@ jerkY2 = np.gradient(accY2)
 jerkZ2 = np.gradient(accZ2)
 jerk1 = np.sqrt(jerkX1**2+jerkY1**2+jerkZ1**2)
 jerk2 = np.sqrt(jerkX2**2+jerkY2**2+jerkZ2**2)
+"""
 
 # Camera Jerk Calcs
 N = 3
@@ -124,16 +139,20 @@ camJerkZ2 = np.gradient(camAccZ2)
 camJerk1 = np.sqrt(camJerkX1**2+camJerkY1**2+camJerkZ1**2)
 camJerk2 = np.sqrt(camJerkX2**2+camJerkY2**2+camJerkZ2**2)
 
+pth1c = 0
+pth2c = 0
+
 #acc1a = np.empty([lngth])
-acc1b = np.empty([lngth])
+#acc1b = np.empty([lngth])
 #acc2a = np.empty([lngth])
-acc2b = np.empty([lngth])
+#acc2b = np.empty([lngth])
 
 #acc1a[0] = np.linalg.norm(acc1[0])
 #acc2a[0] = np.linalg.norm(acc2[0])
-acc1b[0] = np.linalg.norm(np.array([accX1[0],accY1[0],accZ1[0]]))
-acc2b[0] = np.linalg.norm(np.array([accX2[0],accY2[0],accZ2[0]]))
+#acc1b[0] = np.linalg.norm(np.array([accX1[0],accY1[0],accZ1[0]]))
+#acc2b[0] = np.linalg.norm(np.array([accX2[0],accY2[0],accZ2[0]]))
 
+"""
 # Angular Velocity Variability
 data['norm'] = data.apply(lambda row : np.linalg.norm([row['wX1'],row['wY1'],row['wZ1']]),axis=1)
 norm = data['norm'].values[rnge[0]:rnge[1]]
@@ -163,7 +182,7 @@ wvar = norm.mean()
 print("Acceleration Variability 2 Mean: "+str(wvar))
 wdev = norm.std(ddof=0)
 print("Acceleration Variability 2 Stdev: "+str(wdev))
-
+"""
 # Acceleration Variability Camera Data
 wvar = camAcc1[camRnge[0]:camRnge[1]].mean()
 print("Acceleration Variability Cam 1 Mean: "+str(wvar))
@@ -175,6 +194,7 @@ print("Acceleration Variability Cam 2 Mean: "+str(wvar))
 wdev = camAcc2[camRnge[0]:camRnge[1]].std(ddof=0)
 print("Acceleration Variability Cam 2 Stdev: "+str(wdev))
 
+"""
 # Deliberate Hand Movements
 # Count the # times accel > 10
 count1 = 0
@@ -185,8 +205,6 @@ boo2 = False
 # Path Length
 pth1 = 0
 pth2 = 0
-pth1c = 0
-pth2c = 0
 # Initial positions and velocities
 p1x = np.empty([lngth])
 p1y = np.empty([lngth])
@@ -222,8 +240,8 @@ for i in range(rnge[0],rnge[1]-1):
 	# To find Path length
     #pth1 = pth1 + abs(np.linalg.norm(np.array([p1x[j+1]-p1x[j],p1y[j+1]-p1y[j],p1z[j+1]-p1z[j]])))
     #pth2 = pth2 + abs(np.linalg.norm(np.array([p2x[j+1]-p2x[j],p2y[j+1]-p2y[j],p2z[j+1]-p2z[j]])))
-    pth1 = pth1 + np.linalg.norm(np.array([posX1[i+1]-posX1[i],posY1[i+1]-posY1[i],posZ1[i+1]-posZ1[i]]))
-    pth2 = pth2 + np.linalg.norm(np.array([posX2[i+1]-posX2[i],posY2[i+1]-posY2[i],posZ2[i+1]-posZ2[i]]))
+    #pth1 = pth1 + np.linalg.norm(np.array([posX1[i+1]-posX1[i],posY1[i+1]-posY1[i],posZ1[i+1]-posZ1[i]]))
+    #pth2 = pth2 + np.linalg.norm(np.array([posX2[i+1]-posX2[i],posY2[i+1]-posY2[i],posZ2[i+1]-posZ2[i]]))
 
     # Deliberate Hand Movements
     if acc1b[j] >= 10 and not boo1:
@@ -239,72 +257,91 @@ for i in range(rnge[0],rnge[1]-1):
         boo2 = False
 
     j = j+1
+"""
+for j in range(camRnge[0],camRnge[1]-1):
+    pth1c = pth1c + np.linalg.norm(np.array([camX1[j+1]-camX1[j],camY1[j+1]-camY1[j],camZ1[j+1]-camZ1[j]]))
+    pth2c = pth2c + np.linalg.norm(np.array([camX2[j+1]-camX2[j],camY2[j+1]-camY2[j],camZ2[j+1]-camZ2[j]]))
 
-#for j in range(camRnge[0],camRnge[1]-1):
-    #pth1c = pth1c + np.linalg.norm(np.array([camX1[j+1]-camX1[j],camY1[j+1]-camY1[j],camZ1[j+1]-camZ1[j]]))
-    #pth2c = pth2c + np.linalg.norm(np.array([camX2[j+1]-camX2[j],camY2[j+1]-camY2[j],camZ2[j+1]-camZ2[j]]))
-
-print("Path length 1: "+str(pth1))
-print("Path length 2: "+str(pth2))
+#print("Path length 1: "+str(pth1))
+#print("Path length 2: "+str(pth2))
 print("Path length 1 Cam: "+str(pth1c))
 print("Path length 2 Cam: "+str(pth2c))
 
-print("Deliberate count 1: "+str(count1))
-print("Deliberate count 2: "+str(count2))
+#print("Deliberate count 1: "+str(count1))
+#print("Deliberate count 2: "+str(count2))
 
 # Mean Jerk
-mjerk1 = np.mean(jerk1[rnge[0]:rnge[1]])
-print("Mean Jerk 1: "+str(mjerk1))
-mjerk2 = np.mean(jerk2[rnge[0]:rnge[1]])
-print("Mean Jerk 2: "+str(mjerk2))
+#mjerk1 = np.mean(jerk1[rnge[0]:rnge[1]])
+#print("Mean Jerk 1: "+str(mjerk1))
+#mjerk2 = np.mean(jerk2[rnge[0]:rnge[1]])
+#print("Mean Jerk 2: "+str(mjerk2))
 mjerk1c = np.mean(camJerk1[camRnge[0]:camRnge[1]])
 print("Mean Jerk 1 Cam: "+str(mjerk1c))
 mjerk2c = np.mean(camJerk2[camRnge[0]:camRnge[1]])
 print("Mean Jerk 2 Cam: "+str(mjerk2c))
 
 plt.figure(1)
-timep = time[0:len(posX1)]
-timev = time[0:len(velX1)]
-timea = time[0:len(accX1)]
-plt.subplot(3,3,1)
-plt.plot(timep,posX1)
-plt.subplot(3,3,4)
-plt.plot(timev,velX1)
-plt.subplot(3,3,7)
-plt.plot(timea,accX1)
-plt.subplot(3,3,2)
-plt.plot(timep,posY1)
-plt.subplot(3,3,5)
-plt.plot(timev,velY1)
-plt.subplot(3,3,8)
-plt.plot(timea,accY1)
-plt.subplot(3,3,3)
-plt.plot(timep,posZ1)
-plt.subplot(3,3,6)
-plt.plot(timev,velZ1)
-plt.subplot(3,3,9)
-plt.plot(timea,accZ1)
+timep = time[0:len(camX1)]
+timev = time[0:len(camVelX1)]
+timea = time[0:len(camAccX1)]
+timej = time[0:len(camJerkX1)]
+plt.subplot(4,3,1)
+plt.plot(timep[camRnge[0]:camRnge[1]],camX1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,4)
+plt.plot(timev[camRnge[0]:camRnge[1]],camVelX1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,7)
+plt.plot(timea[camRnge[0]:camRnge[1]],camAccX1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,10)
+plt.plot(timej[camRnge[0]:camRnge[1]],camJerkX1[camRnge[0]:camRnge[1]])
 
-time = time[rnge[0]:rnge[1]]
+plt.subplot(4,3,2)
+plt.plot(timep[camRnge[0]:camRnge[1]],camY1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,5)
+plt.plot(timev[camRnge[0]:camRnge[1]],camVelY1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,8)
+plt.plot(timea[camRnge[0]:camRnge[1]],camAccY1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,11)
+plt.plot(timej[camRnge[0]:camRnge[1]],camJerkY1[camRnge[0]:camRnge[1]])
+
+plt.subplot(4,3,3)
+plt.plot(timep[camRnge[0]:camRnge[1]],camZ1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,6)
+plt.plot(timev[camRnge[0]:camRnge[1]],camVelZ1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,9)
+plt.plot(timea[camRnge[0]:camRnge[1]],camAccZ1[camRnge[0]:camRnge[1]])
+plt.subplot(4,3,12)
+plt.plot(timej[camRnge[0]:camRnge[1]],camJerkZ1[camRnge[0]:camRnge[1]])
+#plt.subplot(3,3,5)
+#plt.plot(timev,velY1)
+#plt.subplot(3,3,8)
+#plt.plot(timea,accY1)
+#plt.subplot(3,3,3)
+#plt.plot(timep,posZ1)
+#plt.subplot(3,3,6)
+#plt.plot(timev,velZ1)
+#plt.subplot(3,3,9)
+#plt.plot(timea,accZ1)
+
+time = time[camRnge[0]:camRnge[1]]
 cutoff = np.ones(len(time))*10
 plt.figure(2)
 plt.subplot(2,2,1)
 #plt.plot(time,acc1a,'r',time,acc1b,'r--')
 #plt.legend(('acc 1', 'acc 1 filtered'), loc='best')
-plt.plot(time,acc1b,'r',time,cutoff,'r--')
-plt.legend(('acc 1', 'Deliberate Threshold'), loc='best')
+plt.plot(time,camAcc1[camRnge[0]:camRnge[1]],'r')#,time,cutoff,'r--')
+#plt.legend(('acc 1', 'Deliberate Threshold'), loc='best')
 plt.subplot(2,2,2)
 #plt.plot(time,acc1a,'b',time,acc1b,'k')
 #plt.legend(('acc 2', 'acc 2 filtered'), loc='best')
-plt.plot(time,acc2b,'b',time,cutoff,'k')
-plt.legend(('acc 2', 'Deliberate Threshold'), loc='best')
+plt.plot(time,camAcc2[camRnge[0]:camRnge[1]],'b')#,time,cutoff,'k')
+#plt.legend(('acc 2', 'Deliberate Threshold'), loc='best')
 
-mjerk1 = np.ones(len(time))*mjerk1
-mjerk2 = np.ones(len(time))*mjerk2
+mjerk1 = np.ones(len(time))*mjerk1c
+mjerk2 = np.ones(len(time))*mjerk2c
 plt.subplot(2,2,3)
-plt.plot(time,jerk1[rnge[0]:rnge[1]],'r',time,mjerk1,'r--')
+plt.plot(time,camJerk1[camRnge[0]:camRnge[1]],'r',time,mjerk1,'r--')
 plt.legend(('jerk 1', 'mean jerk'), loc='best')
 plt.subplot(2,2,4)
-plt.plot(time,jerk2[rnge[0]:rnge[1]],'b',time,mjerk2,'k')
+plt.plot(time,camJerk2[camRnge[0]:camRnge[1]],'b',time,mjerk2,'k')
 plt.legend(('jerk 2', 'mearn jerk'), loc='best')
 plt.show()
